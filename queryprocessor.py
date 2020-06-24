795fcbbb5919e62c9b27132264e754a967046ba7
Onur Guzel <onurguzel96@hotmail.co.uk>
2020-06-24 10:26:46 +0100

    query 1

--- queryprocessor.py
+++ queryprocessor.py
@@ -2,3 +2,8 @@ def process_query(query):
 	if ("romeo and juliet" in query.lower()):
 		return "William Shakespeare"
 	return ""
+
+def process_query1(query):
+	if ("%20258" in query.lower()):
+		return "258"
+	return ""
